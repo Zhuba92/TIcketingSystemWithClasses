49c18fe6b7ae68ce6380dda1527e935d66cbcb12
zachary <zhuba@my.wctc.edu>
2021-02-24 17:17:21 -0600

    A few improvements

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace TicketingWithClasses
     {
         static void Main(string[] args)
         {
-            Console.Write("Would you like to create a ticket (Y/N)?");
+            Console.Write("Would you like to create a ticket (Y/N)? ");
             char choice = Console.ReadLine().ToUpper()[0];
             string file = "Tickets.csv";
             StreamWriter sw = new StreamWriter(file);
@@ -16,40 +16,40 @@ namespace TicketingWithClasses
 
             do
             {
-                Console.WriteLine("Enter a ticket number");
+                Console.Write("Enter a ticket number: ");
                 ticket.ticketNumber = Console.ReadLine();
 
-                Console.WriteLine("Enter a summary");
+                Console.Write("Enter a summary: ");
                 ticket.ticketSummary = Console.ReadLine();
 
-                Console.WriteLine("Enter a status");
+                Console.Write("Enter a status: ");
                 ticket.ticketStatus = Console.ReadLine();
 
-                Console.WriteLine("Enter a priority");
+                Console.Write("Enter a priority: ");
                 ticket.ticketPriority = Console.ReadLine();
 
-                Console.WriteLine("Enter a submitter");
+                Console.Write("Enter a submitter: ");
                 ticket.ticketSubmitter = Console.ReadLine();
 
-                Console.WriteLine("Enter an assigned to");
+                Console.Write("Enter an assigned to: ");
                 ticket.ticketAssignedTo = Console.ReadLine();
 
-                Console.WriteLine("Would you like to add a watcher (Y/N)? ");
+                Console.Write("Would you like to add a watcher (Y/N)? ");
                 char addWatcher = Console.ReadLine().ToUpper()[0];
                 
                 do
                 {
-                    Console.Write("What is the name of the watcher? ");
+                    Console.Write("Enter the name of the watcher: ");
                     string addName = Console.ReadLine();
                     ticket.ticketWatchers.Add(addName);
-                    Console.WriteLine("Would you like to add another watcher (Y/N)? ");
+                    Console.Write("Would you like to add another watcher (Y/N)? ");
                     addWatcher = Console.ReadLine().ToUpper()[0];
 
                 } while (addWatcher != 'N');
 
                 sw.WriteLine(ticket.Display());
                 sw.Close();
-                Console.WriteLine("Would you like to create another ticket (Y/N)? ");
+                Console.Write("Would you like to create another ticket (Y/N)? ");
                 choice = Console.ReadLine().ToUpper()[0];
 
             } while (choice != 'N');
